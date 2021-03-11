d027d712e5b098505e2f308ea64c15c4706fbbb9
rangaswamy <rangaswamy@gmail.com>
2021-03-12 01:13:39 +0530

    changes made to april rel

--- first.java
+++ first.java
@@ -1,6 +1,8 @@
 import java.util*;
 class first{
      public static void main(String args[]){
+              int a, b, c;
      System.out.println("Hello java");
+     System.out.println("This is my April-release code");
      }
      }
